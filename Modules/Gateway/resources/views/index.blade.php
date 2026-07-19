d70e07215226c86944b72395148e333eb4c26974
saeed dehghan <saeeddehghans31@gmail.com>
2026-07-19 10:31:08 +0330

    :

--- Modules/Gateway/resources/views/index.blade.php
+++
@@ -1,5 +0,0 @@
-<x-gateway::layouts.master>
-    <h1>Hello World</h1>
-
-    <p>Module: {!! config('gateway.name') !!}</p>
-</x-gateway::layouts.master>
--- Modules/Gateway/routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Modules\Gateway\Http\Controllers\GatewayController;
-
-Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
-    Route::apiResource('gateways', GatewayController::class)->names('gateway');
-});
